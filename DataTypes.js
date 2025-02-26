5cc1e49628ba8b5781d1ae7936e763971bcb704f
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-25 19:33:58 -0800

    added file

--- DataTypes.js
+++ DataTypes.js
@@ -9,3 +9,13 @@ console.log(value)
 
 const a = 5 + 10
 console.log(a)
+
+//দুটি সংখ্যা গুণ করো: 4 * 7
+
+const x = 4 * 7
+console.log(x)
+
+//একটি সংখ্যা ইনপুট নিয়ে তার দ্বিগুণ করো।
+
+const num = 20 * 2
+console.log(num)
