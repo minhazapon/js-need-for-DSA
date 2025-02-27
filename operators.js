583244364562fea183d11882f827d87a048c01e1
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-27 05:18:12 -0800

    added file

---
+++ operators.js
@@ -0,0 +1,19 @@
+
+
+//arithmetic operator
+
+const num = 1000;
+const num1 = num * 10000
+const num2 = num1 / 200000;
+const num3 = num2 - 1000
+const num4 = num3 + 1000 - 1
+const allNum = { num4 }
+
+console.log(allNum)
+
+if (allNum < 7000) {
+    console.log('is good baby')
+} else {
+    console.log('is not good baby')
+}
+
