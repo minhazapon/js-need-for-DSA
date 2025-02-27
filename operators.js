c5a40221248104433d393452b46e2008561dfe58
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-27 05:25:04 -0800

    added file

--- operators.js
+++ operators.js
@@ -17,3 +17,30 @@ if (allNum < 7000) {
     console.log('is not good baby')
 }
 
+//Comparison Operators
+
+const isNumber = 2000;
+
+const eq = isNumber > 1000;
+console.log(eq)
+
+const e3 = isNumber === 2000
+console.log(e3)
+
+const b1 = isNumber < 3000;
+console.log(b1)
+
+const e2 = isNumber == 100;
+console.log(e2)
+
+const eb = isNumber >= 1000
+console.log(eb)
+
+const ebb = isNumber <= 1500;
+console.log(ebb)
+
+const ecc = isNumber != 20
+console.log(ecc)
+
+const exx = isNumber !== 300
+console.log(exx)
