7ac4d1adc19a330271a27f34221836773d4d0cda
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-28 05:58:45 -0800

    added file

--- Loop.js
+++ Loop.js
@@ -1,56 +1,66 @@
 
-//for loop
+// //for loop
 
-for (let i = 10; i < 100; i++) {
+// for (let i = 10; i < 100; i++) {
 
-    const value = i
-    console.log(value)
+//     const value = i
+//     console.log(value)
 
-}
+// }
 
-for (let i = 100; i < 1000; i++) {
+// for (let i = 100; i < 1000; i++) {
 
-    const value = i
-    console.log(value)
+//     const value = i
+//     console.log(value)
 
-}
+// }
 
-for (let i = 2; i < 20 + 4; i++) {
+// for (let i = 2; i < 20 + 4; i++) {
 
-    const value = i
-    console.log(value)
+//     const value = i
+//     console.log(value)
 
-}
+// }
 
-let arrValue = [20, 30, 40, 50, 60, 70, 80, 900]
+// let arrValue = [20, 30, 40, 50, 60, 70, 80, 900]
 
-for (let i = 0; i < arrValue.length; i++) {
+// for (let i = 0; i < arrValue.length; i++) {
 
-    const value = arrValue
-    console.log(value[i])
+//     const value = arrValue
+//     console.log(value[i])
 
-}
+// }
 
-//while
+// //while
 
-let v1 = 10;
+// let v1 = 10;
 
-while (v1 < 100) {
+// while (v1 < 100) {
 
-    const vv = v1
-    console.log(vv)
-    v1++
+//     const vv = v1
+//     console.log(vv)
+//     v1++
 
-}
+// }
 
-let i = 20;
+// let i = 20;
 
-while (i < 200) {
+// while (i < 200) {
 
-    const value = i
-    console.log(value)
-    i++
+//     const value = i
+//     console.log(value)
+//     i++
 
-}
+// }
 
 //problem solving section
+
+//📌 For Loop
+
+//1️⃣ ১ থেকে ১০ পর্যন্ত সংখ্যা প্রিন্ট করো।
+
+for (let i = 1; i < 11; i++) {
+
+    console.log(i)
+
+}
