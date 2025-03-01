470f82e6f610a404f711f30a996e97fc4a36361a
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-01 03:28:29 -0800

    added file

---
+++ object.js
@@ -0,0 +1,18 @@
+
+
+const aponInformation = {
+
+    name: "minhazul abedin apon",
+    age: 23,
+    number: 1950374409,
+    profession: "software developer",
+    country: "bangladesh",
+    district: "jhenaidah",
+    location: "jhenaidah sadar",
+    company: ` microsoft google toptal `
+
+}
+
+const { name, age, number, profession, country, district, location, company } = aponInformation
+
+console.log(name, age, number, profession, country, district, location, company)
