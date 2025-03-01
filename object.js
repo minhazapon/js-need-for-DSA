375ab1de2b3e007076a75f1fdcb3dfadf051ac14
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-01 03:41:32 -0800

    added file

--- object.js
+++ object.js
@@ -16,3 +16,18 @@ const aponInformation = {
 const { name, age, number, profession, country, district, location, company } = aponInformation
 
 console.log(name, age, number, profession, country, district, location, company)
+
+
+// //1️⃣ Object.keys()
+// 🔹 এটি একটি অবজেক্টের সব কী(property names) কে অ্যারে আকারে রিটার্ন করে।
+
+const allInfo = {
+
+    skill: "js",
+    skill1: "ts",
+    skill2: "react.js",
+    skill3: "DSA"
+
+}
+
+console.log(Object.keys(allInfo))
