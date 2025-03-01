2c0614568a7fccab970955d5f9701980f3d7e1f5
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-28 20:33:42 -0800

    added file

--- es6SRD.js
+++ es6SRD.js
@@ -8,3 +8,9 @@ const values = [1, 2, 3, 4, 5, 6, 7]
 const allValues = [...values, 50, 1000]
 
 console.log(allValues)
+
+const A = { name: "zzz", age: 50 }
+
+const B = { ...A, location: 'jhenaidah' }
+
+console.log(B)
