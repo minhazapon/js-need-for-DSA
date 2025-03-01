82b2dc7c517650c57d1ca1e7d208f1cc04e3f547
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-01 04:54:49 -0800

    added file

--- ArrayMethods.js
+++ ArrayMethods.js
@@ -8,3 +8,20 @@ const value = [1, 2, 3, 4, 5, 6, 7, 8]
 const pushValue = value.push(10)
 
 console.log(pushValue)
+
+//pop
+
+const popValue = ["apon", "shemanto", "alamin", "limon", "munna"]
+
+const pValue = popValue.pop();
+
+console.log(pValue)
+
+// 3. shift()
+// ➡️ এটি একটি অ্যারের প্রথম এলিমেন্ট সরিয়ে ফেলে এবং সেটি রিটার্ন করে।
+
+const sValue = [22, 3445, 45, 5, 5656]
+
+sValue.shift();
+
+console.log(sValue)
